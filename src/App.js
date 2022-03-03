b5f6737f8fd545fa093b98f9cb662c606cadf466
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-02 17:43:19 -0700

    Update app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React, { Component } from 'react';
+import Areas from './Areas';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      areas: ['kitchen', 'bathroom', 'bedroom', 'miscellaneous']
+
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello hello!</h1>
+        <Areas areas={this.state.areas} />
+      </div>
+    )
+  }
 }
 
 export default App;
+
+//The user comes onto the app
+//They see cards that have areas of the house on them (Kitchen, Bathroom, Etc), which will be clickable links.
+//You want to pass the categories down to cards, in order to... add them on AreaCards
+//They will link to different projects they might have (Leaky faucet, clogged toilet)
+//They will click on ONE project, which will also be a link, which will lead them to
+//A youtube video.
