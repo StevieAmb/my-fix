1b95c93ab293122a4e612627db167e87730ba2d4
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-02 17:41:29 -0700

    Create file for area cards

---
+++ src/AreaCards.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './AreaCards.css'
+
+const AreaCards = ({homeArea}) => {
+  return (
+    <div className="area-card">
+    <h1>{homeArea}</h1>
+    </div>
+  )
+}
+
+
+export default AreaCards;
