e241292a617eebf54caa53b4b95842ed9bcf481d
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-02 21:43:29 -0700

    Create cards for individual projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Project from './Project';
 
 const Projects = ({category}) => {
-  
+  {console.log(category)}
+  let allProjects = category.map(category => {
+    return (
+      <Project 
+        key={category.name}
+        id={category.id}
+        name={category.Project}
+      />
+    )
+  })
   return (
-    <h1>{category.project}</h1>
+    <div>
+      {allProjects}
+    </div>
   )
 }
 
