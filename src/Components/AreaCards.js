448946cfa864502981cf19d86bb8ac63e22c96af
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 01:09:34 -0700

    Add prop types to file

--- src/Components/AreaCards.js
+++ src/Components/AreaCards.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../AreaCards.css'
+import PropTypes from 'prop-types'
 import { NavLink } from "react-router-dom";
 import bedroom from '../bedroom.jpeg';
 import bathroom from '../bathroom.jpeg';
@@ -27,3 +28,7 @@ const AreaCards = ({homeArea}) => {
 
 
 export default AreaCards;
+
+AreaCards.propTypes = {
+  homeArea: PropTypes.string.isRequired
+}
