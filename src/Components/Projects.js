79e0308cb99efb0656f066710c1dcee285bc2c5f
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 16:32:44 -0700

    Clean up and structure file

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Project from './Project';
-import '../Projects.css'
+import '../Projects.css';
 
 const Projects = ({category}) => {
   let allProjects = category.map(category => {
@@ -13,7 +13,7 @@ const Projects = ({category}) => {
     )
   })
   return (
-    <div className='projects-container'>
+    <div className="projects-container">
       {allProjects}
     </div>
   )
@@ -21,5 +21,3 @@ const Projects = ({category}) => {
 
 export default Projects;
 
-//I am going add a NavLink to the projects card
-//The NavLink is going to go to a new path that is going to render the video component
