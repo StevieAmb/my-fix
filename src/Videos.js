291f1445fd70c544a67b07c26e66eb8df1e2a5e2
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 01:07:31 -0700

    Add prop types to file

--- src/Videos.js
+++ src/Videos.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Video from './Video';
 
 const Videos = ({repairVideo}) => {
@@ -11,3 +12,7 @@ const Videos = ({repairVideo}) => {
   }
 
 export default Videos
+
+Videos.propType = {
+  repairVideo: PropTypes.object.isRequired
+}
