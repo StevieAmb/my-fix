0ffec0902c1fd0210a34dbe6b375e7f138dffc2b
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 16:33:12 -0700

    Clean up and structure project file

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,13 +1,13 @@
-import React from "react";
-import PropTypes from 'prop-types'
-import { NavLink } from 'react-router-dom'
-import '../Project.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import '../Project.css';
 
 const Project = ({name}) => {
   return (
     <NavLink className="project-card" to={`/video/${name}`}>
       <h1>{name}</h1>
-      </NavLink>
+    </NavLink>
   )
 }
 
