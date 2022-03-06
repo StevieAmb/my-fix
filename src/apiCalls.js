a99b70d4c5ff73e102c1d82d951aa2e6e54d70f2
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-05 21:44:11 -0700

    Add different throw errors to different response codes

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,18 @@
 const getHomeRepairs = () => {
   return fetch('https://young-everglades-39392.herokuapp.com/')
-  .then(response => response.json())
+  .then(response => {
+    if(!response.ok) {
+      if(response.status >= 500) {
+        throw new Error(`${response.status}. I am so sorry for the inconvenience! The server is down. Please try again, later.`)
+      } else if (response.status === 404) {
+        throw new Error(`${response.status}. We apologize for the inconvenience. Please try again, or go back to the Main Page.`)
+      } else {
+        throw new Error(`${response.status}. We apologize for the inconvenience. Please try again later.`)
+      }
+    } else {
+      return response.json()
+    }
+  })
 }
 
 export default getHomeRepairs
