0ba4a4ee2ca2ab0042cb3901137f7122f34df8cc
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 13:09:32 -0700

    Re-arrange button for video

--- src/Video.js
+++ src/Video.js
@@ -2,16 +2,16 @@ import React from "react";
 import PropTypes from 'prop-types'
 import './Video.css'
 
-const Video = ({video, projToTry}) => {
-  console.log("video", video)
+const Video = ({video, projToTry, name}) => {
   return ( 
-    <div>
-       <button onClick={() => projToTry(video)}>I'm Gonna Try This</button>
+    <div className="video-page-container">
       <div className="video-container">
+        <h3>{name}</h3>
         <iframe width="420" height="315"
           src={video}>
         </iframe>
       </div>
+        <button onClick={() => projToTry(video)}>Add To My Projects</button>
     </div>
   )
 }
