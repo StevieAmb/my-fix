4346c20e60050a3e790fbb537df756a240b86066
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 13:12:08 -0700

    Test for navbar on tryProject page

--- cypress/integration/tryProjectsPage_spec.js
+++ cypress/integration/tryProjectsPage_spec.js
@@ -1,4 +1,4 @@
-describe('Main Page', () => {
+describe('Add To My Projects', () => {
   beforeEach(() => {
     cy.intercept('GET', "https://young-everglades-39392.herokuapp.com/", {fixture: 'example.json'})
     cy.visit('http://localhost:3000/')
@@ -10,6 +10,16 @@ describe('Main Page', () => {
     .url('http://localhost:3000/video/Leaky%20Faucet')
   })
 
+  it('User should see the nav bar, with title, blurb', () => {
+    cy.get('.nav-bar').contains('MY FIXINGS')
+    .contains('For those who need to justify the amount of tools they own...')
+  })
+
+  it('User should see "My Projects" button on nav bar', () => {
+    cy.get('.nav-bar')
+    .get('button').contains('My Projects').click()
+  })
+
   it('User should see error message if they have not added any projects to try yet', () => {
     cy.get('.nav-bar')
     .get('button').contains('My Projects').click()
@@ -18,18 +28,18 @@ describe('Main Page', () => {
     .contains('It seems you have not added a project yet. Get your tools and get going!')
   })
 
-  it('User should be able to see and click the button for trying a project', () => {
+  it('User should be able to see and click the button for adding a project to their My Projects', () => {
     cy.get('button').contains('Add To My Projects').click()
   })
 
-  it('User should be able to click "My Projects" button on navbar and see a list of project they want to try', () => {
+  it('User should be able to click "My Projects" button on navbar and see a list of project they added', () => {
     cy.get('button').contains('Add To My Projects').click()
     cy.get('.nav-bar')
     .get('button').contains('My Projects').click()
     .url(' http://localhost:3000/tryThis')
   })
 
-  it('User should be able to click items on the list to see the video again', () => {
+  it('User should be able to click items on the list they made to see the video again', () => {
     cy.get('button').contains('Add To My Projects').click()
     cy.get('.nav-bar')
     .get('button').contains('My Projects').click()
