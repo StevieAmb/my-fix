e9c9899fc8c989796cd9aec296a13c56835e9eb6
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 13:11:06 -0700

    Update error message

--- src/CatchError.js
+++ src/CatchError.js
@@ -5,7 +5,7 @@ const CatchError = ({error}) => {
   return (
       <div className="catch-error">
     <h3>{error}</h3>
-    <p>Something is clearly broken. Maybe you can reach down into your toolbox and help us fix it?</p>
+    <p>Something is clearly broken. Click "My FIXINGS" in the grey bar above and see if you can fix it for me, will ya? </p>
       </div>
   )
 }
