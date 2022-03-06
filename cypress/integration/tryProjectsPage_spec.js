38aa97910a6ac576cc88365f7ce30fccea6c0241
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-06 10:49:29 -0700

    Finalize try page testing

--- cypress/integration/tryProjectsPage_spec.js
+++ cypress/integration/tryProjectsPage_spec.js
@@ -10,14 +10,33 @@ describe('Main Page', () => {
     .url('http://localhost:3000/video/Leaky%20Faucet')
   })
 
+  it('User should see error message if they have not added any projects to try yet', () => {
+    cy.get('.nav-bar')
+    .get('button').contains('My Projects').click()
+    .url('http://localhost:3000/tryThis')
+    .get('h4')
+    .contains('It seems you have not added a project yet. Get your tools and get going!')
+  })
+
   it('User should be able to see and click the button for trying a project', () => {
-    cy.get('button').contains('Gonna Try This').click()
+    cy.get('button').contains('Add To My Projects').click()
   })
 
   it('User should be able to click "My Projects" button on navbar and see a list of project they want to try', () => {
-    cy.get('button').contains('Gonna Try This').click()
+    cy.get('button').contains('Add To My Projects').click()
     cy.get('.nav-bar')
     .get('button').contains('My Projects').click()
     .url(' http://localhost:3000/tryThis')
+  })
+
+  it('User should be able to click items on the list to see the video again', () => {
+    cy.get('button').contains('Add To My Projects').click()
+    cy.get('.nav-bar')
+    .get('button').contains('My Projects').click()
+    .url(' http://localhost:3000/tryThis')
+    cy.get('.try-container')
+    .children('a')
+    .contains('Leaky Faucet').click()
+    .url('')
   })
 })
