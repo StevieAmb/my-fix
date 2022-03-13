066285ede61df2217deb25b8e19ed50ac5e4a280
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-12 19:53:47 -0700

    Update tries file

--- src/Tries.js
+++ src/Tries.js
@@ -3,18 +3,20 @@ import Project from './Components/Project';
 import PropTypes from 'prop-types';
 
 const Tries = ({category}) => {
+  console.log(category)
   let toTries = category.map(category => {
     return (
       <Project
         key={category.id}
         id={category.id}
         name={category.project}
+        tools={category.toolsNeeded}
         />
     )
   })
   return (
     <section className="try-container">
-      {toTries}
+          {toTries}
     </section>
   )
 }
