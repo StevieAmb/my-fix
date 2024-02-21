4fb216a8c28908dad4306c9a221a9246daa22c89
Stevie Ambroise <s.ambroise@protonmail.com>
2024-02-21 10:42:34 -0700

    Remove static html width

--- src/Components/AreaCards.js
+++ src/Components/AreaCards.js
@@ -11,7 +11,7 @@ const AreaCards = ({homeArea}) => {
   let images = [bedroom, bathroom, kitchen, miscellaneous]
   let areaPics = images.map(image => {
     if(image.includes(homeArea)) {
-      return (<img key={image} className="area-photo" src={`${image}`} alt={homeArea} width='450' height='380'/>)
+      return (<img key={image} className="area-photo" src={`${image}`} alt={homeArea}/>)
     } 
   })
 
